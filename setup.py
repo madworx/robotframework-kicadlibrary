d5169aa8dce5a55f5054ecc04de02dee35a2b7d8
Martin Kjellstrand <martin.kjellstrand@madworx.se>
2018-12-10 18:40:10 +0100

    Enable building with Travis and upload artifacts to GitHub releases.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 """Setup file for Python"""
 
 import sys
+import os
 
 from setuptools import setup, find_packages
+
+from src.robotdoc.libdocpkg import setup_command
 
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner','pytest<5'] if needs_pytest else []
@@ -17,9 +20,10 @@ setup(name='robotframework-kicadlibrary',
       packages=find_packages('./src'),
       package_dir={'': 'src'},
       install_requires=['robotframework<=3.0.4'],
-      setup_requires=['setuptools-git-version'] + pytest_runner,
+      setup_requires=['setuptools-git-version','robotframework<=3.0.4'] + pytest_runner,
       tests_require=['pytest-runner','pytest<5', 'pytest-cov', 'pytest-mock',
                      'pylint<2.0.0', 'pytest-pylint', 'coverage', 'pytest-html',
-                     'setuptools-git-version'],
-      test_suite='tests'
+                     'setuptools-git-version','robotframework<=3.0.4'],
+      test_suite='tests',
+      cmdclass     = {'build_rf_docs': setup_command.BuildLibDoc}
 )
