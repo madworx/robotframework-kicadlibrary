17a5e46796dd8bae7732964682d605b9f2107aba
Martin Kjellstrand <martin.kjellstrand@madworx.se>
2018-12-16 20:46:34 +0100

    .

--- bin/deploy-to-dockerhub.sh
+++ bin/deploy-to-dockerhub.sh
@@ -4,10 +4,10 @@ export TAGGED_VERSION="$(git describe --exact-match HEAD 2>/dev/null)"
 export DOCKER_USERNAME="$1"
 export DOCKER_PASSWORD="$2"
 
-if [[ ! -z '${TAGGED_VERSION}' ]] ; then
+if [[ ! -z "${TAGGED_VERSION}" ]] ; then
     echo "We are on par with a tagged release -- deploying to github."
     source <(curl 'https://raw.githubusercontent.com/madworx/cd-ci-glue/master/src/cd-ci-glue.bash')
-    dockerhub_push_image 'madworx/robotframework-kicadlibrary:${TAGGED_VERSION}'
+    dockerhub_push_image "madworx/robotframework-kicadlibrary:${TAGGED_VERSION}"
     dockerhub_set_description 'madworx/robotframework-kicadlibrary' README.md
 fi
 
