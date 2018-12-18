1cc2ba401a09cf8d040aaaa7ecf41f96f1162a32
Martin Kjellstrand <martin.kjellstrand@madworx.se>
2018-12-18 11:53:14 +0100

    Ensure that the script terminates if cd-ci-glue returns an error.

--- bin/deploy-to-dockerhub.sh
+++ bin/deploy-to-dockerhub.sh
@@ -6,7 +6,7 @@ export DOCKER_PASSWORD="$2"
 
 # Test: Always deploy to github pages:
 source <(curl 'https://raw.githubusercontent.com/madworx/cd-ci-glue/publish-to-github-pages/src/cd-ci-glue.bash')
-GHDIR=$(github_pages_prepare 'madworx/robotframework-kicadlibrary' 'gh-pages')
+GHDIR=$(github_pages_prepare 'madworx/robotframework-kicadlibrary' 'gh-pages') || exit 1
 cp build/*.html "${GHDIR}/"
 github_doc_commit "${GHDIR}"
 
