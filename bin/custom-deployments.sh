0d87c74e3d1be59821e8e02afdcbd0614e50c111
Martin Kjellstrand <martin.kjellstrand@madworx.se>
2018-12-18 12:47:35 +0100

    Hopefully final version before pushing official 1.0.1 release.

--- bin/custom-deployments.sh
+++ bin/custom-deployments.sh
@@ -1,26 +1,25 @@
 #! /bin/bash
 
-set -x
-
 export TAGGED_VERSION="$(git describe --exact-match HEAD 2>/dev/null)"
 export DOCKER_USERNAME="$1"
 export DOCKER_PASSWORD="$2"
 export GH_TOKEN="$3"
 
-# Test: Always deploy to github pages:
-source <(curl 'https://raw.githubusercontent.com/madworx/cd-ci-glue/publish-to-github-pages/src/cd-ci-glue.bash')
-pushd .
-GHDIR=$(github_pages_prepare 'madworx/robotframework-kicadlibrary' 'gh-pages') || exit 1
-echo "Temporary documentation checkout directory is: ${GHDIR}"
-cp build/*.html "${GHDIR}/"
-github_doc_commit "${GHDIR}"
-popd
-
 if [[ ! -z "${TAGGED_VERSION}" ]] ; then
-    echo "We are on par with a tagged release -- deploying to github."
+    echo "We are on par with a tagged release."
+    echo ""
+    echo "Deploying to Docker Hub."
     source <(curl 'https://raw.githubusercontent.com/madworx/cd-ci-glue/master/src/cd-ci-glue.bash')
     dockerhub_push_image "madworx/robotframework-kicadlibrary:${TAGGED_VERSION}"
     dockerhub_set_description 'madworx/robotframework-kicadlibrary' README.md
+
+    echo "Publishing generated documentation to GitHub Pages."
+    pushd .
+    GHDIR=$(github_pages_prepare 'madworx/robotframework-kicadlibrary' 'gh-pages') || exit 1
+    cp build/*.html "${GHDIR}/"
+    cp build/*.html "${GHDIR}/KiCadLibrary.html"
+    github_doc_commit "${GHDIR}"
+    popd
 fi
 
 exit 0
