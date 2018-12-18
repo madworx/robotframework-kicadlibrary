52a7e779e2bb0fd10b78cdee54ab1a39cd8a228d
Martin Kjellstrand <github@madworx.se>
2018-12-18 13:03:21 +0100

    Deploy documentation to GitHub Pages (#8)
    * Deploy generated documentation to GitHub Pages.
    * Updated link to Keyword documentation in README.md.

--- bin/custom-deployments.sh
+++ bin/custom-deployments.sh
@@ -3,12 +3,23 @@
 export TAGGED_VERSION="$(git describe --exact-match HEAD 2>/dev/null)"
 export DOCKER_USERNAME="$1"
 export DOCKER_PASSWORD="$2"
+export GH_TOKEN="$3"
 
 if [[ ! -z "${TAGGED_VERSION}" ]] ; then
-    echo "We are on par with a tagged release -- deploying to github."
+    echo "We are on par with a tagged release."
+    echo ""
+    echo "Deploying to Docker Hub."
     source <(curl 'https://raw.githubusercontent.com/madworx/cd-ci-glue/master/src/cd-ci-glue.bash')
     dockerhub_push_image "madworx/robotframework-kicadlibrary:${TAGGED_VERSION}"
     dockerhub_set_description 'madworx/robotframework-kicadlibrary' README.md
+
+    echo "Publishing generated documentation to GitHub Pages."
+    pushd .
+    GHDIR=$(github_pages_prepare 'madworx/robotframework-kicadlibrary' 'gh-pages') || exit 1
+    cp build/*.html "${GHDIR}/"
+    cp build/*.html "${GHDIR}/KiCadLibrary.html"
+    github_doc_commit "${GHDIR}"
+    popd
 fi
 
 exit 0
